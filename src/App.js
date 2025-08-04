792feb03e2f27757f6bcf3a4e7497070d5e09e98
Adhiraj Choudhury <adhirajchoudhury123@gmail.com>
2025-08-04 22:49:17 +0530

    this is change first part

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is depo project for testing purposes.
+          <br />
+          It is a simple React application that serves as a template.
+          <br />
+          You can modify it to suit your needs.
         </p>
         <a
           className="App-link"
